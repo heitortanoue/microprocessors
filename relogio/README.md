34a06cc73b76c42e3bc51ab0ca6831daaa2a6f77
Heitor Tanoue <heitortanoue@usp.br>
2023-10-12 19:24:34 -0300

    relogio

---
+++ relogio/README.md
@@ -0,0 +1,37 @@
+# Programa em Assembly para 8051 - Contador com Display de 7 Segmentos
+
+Este é um programa em Assembly para o microcontrolador 8051 que implementa um contador com um display de 7 segmentos. O programa atende aos seguintes requisitos:
+
+- Ao pressionar o botão/chave SW0, o display de 7 segmentos mostrará a contagem de 0 a 9 em loop, com um intervalo de tempo de 0,25 segundos entre cada número.
+- Ao pressionar o botão/chave SW1, o intervalo de tempo para a contagem é alterado para 1 segundo, tornando a contagem mais rápida.
+- Pressionando novamente SW0, a contagem retorna ao intervalo de 0,25 segundos, mantendo a execução do programa em loop.
+- O programa não inicia automaticamente quando executado pela primeira vez; o display está desligado. A contagem começa apenas quando SW0 ou SW1 é pressionado.
+- O programa utiliza sub-rotinas de delay para criar os intervalos de tempo e instruções condicionais para verificar o acionamento das chaves.
+- Um display de 7 segmentos e dois botões (SW0 e SW1) disponíveis no simulador EdSim51 são usados.
+
+### Instruções de Uso:
+
+1. Carregue o programa no simulador EdSim51.
+2. Execute o programa.
+3. Pressione o botão SW0 para iniciar a contagem com um intervalo de tempo de 0,25 segundos.
+4. Pressione o botão SW1 para alternar para um intervalo de tempo de 1 segundo.
+5. Pressione novamente SW0 para retornar ao intervalo de 0,25 segundos.
+6. O contador continuará em loop.
+
+### Configuração de Hardware:
+
+- O botão SW0 deve estar conectado ao pino P2.0.
+- O botão SW1 deve estar conectado ao pino P2.1.
+- O display de 7 segmentos deve estar conectado ao Port 1 (P1).
+
+### Observações:
+
+- Este código é um esboço geral e pode precisar de adaptações específicas para funcionar no simulador EdSim51 e com seu hardware.
+- Certifique-se de ler a documentação do simulador EdSim51 para entender como configurar os pinos de entrada e saída.
+
+## Autores
+👤 [Beatriz Cardoso de Oliveira](https://github.com/trizcard) - NUSP: 12566400
+
+👤 [Heitor Tanoue de Mello](https://github.com/heitortanoue) - NUSP: 12547260
+
+👤 [Guilherme Augusto de Oliveira](https://github.com/GuilhermeAMoreira) - NUSP:
---
+++ relogio/relogio.asm
@@ -0,0 +1,110 @@
+ORG 0x00
+
+; Definição dos registradores
+SW0     equ P2.0        ; SW0 está conectado ao pino P1.0
+SW1     equ P2.1        ; SW1 está conectado ao pino P1.1
+Display equ P1          ; Display de 7 segmentos está conectado ao Port 2
+
+; Definição de constantes
+Delay025s   equ 250     ; Delay para 0,25 segundos
+Delay1s     equ 1000    ; Delay para 1 segundo
+
+ZERO	equ	0C0h
+UM		equ	0F9h
+DOIS	equ	0A4h
+TRES	equ	0b0h
+QUATRO	equ	99h
+CINCO	equ	92h
+SEIS	equ	82h
+SETE	equ	0F8h
+OITO	equ	80h
+NOVE	equ	90h
+
+; Inicialização
+mov P2, #0xFF           ; Configura P1 como entrada com pull-up
+mov Display, #0xFF      ; Desliga o display
+
+MainLoop:
+  mov R1, #0            ; Reseta o valor que vai para o display
+  jmp CountLoop         ; Pula para a sub-rotina de contagem
+
+CountLoop:
+  mov A, R1             ; Move o valor de R1 para o acumulador
+  acall DisplayNumber   ; Mostra o número no display
+  inc R1                ; Incrementa o valor de R1
+  acall Delay           ; Delay de x segundos
+
+  cjne R1, #10, CountLoop ; Loop de contagem de 0 a 9
+  sjmp MainLoop         ; Retorna para o loop principal
+
+DisplayNumber:
+  MOV Display, #0xff    ; Desliga o display
+  cjne A, #0, T1        ; Verifica se o valor é 0
+  mov Display, #ZERO    ; Mostra 0 no display
+  RET                   ; Retorna para quem chamou a função
+
+T1:
+  cjne A, #1, T2        ; Verifica se o valor é 1
+  mov Display, #UM      ; Mostra 1 no display
+  RET
+
+T2:
+  cjne A, #2, T3        ; Verifica se o valor é 2
+  mov Display, #DOIS    ; Mostra 2 no display
+  RET
+
+T3:
+  cjne A, #3, T4        ; Verifica se o valor é 3
+  mov Display, #TRES    ; Mostra 3 no display
+  RET
+
+T4:
+  cjne A, #4, T5        ; Verifica se o valor é 4
+  mov Display, #QUATRO  ; Mostra 4 no display
+  RET
+
+T5:
+  cjne A, #5, T6        ; Verifica se o valor é 5
+  mov Display, #CINCO   ; Mostra 5 no display
+  RET
+
+T6:
+  cjne A, #6, T7        ; Verifica se o valor é 6
+  mov Display, #SEIS    ; Mostra 6 no display
+  RET
+
+T7:
+  cjne A, #7, T8        ; Verifica se o valor é 7
+  mov Display, #SETE    ; Mostra 7 no display
+  RET
+
+T8:
+  cjne A, #8, T9        ; Verifica se o valor é 8
+  mov Display, #OITO    ; Mostra 8 no display
+  RET
+
+T9:
+  mov Display, #NOVE    ; Mostra 9 no display
+  RET
+
+; Implementação da subrotina de Delay
+Delay:
+    JNB SW1, Inicio_1s
+    JNB SW0, Inicio_25s
+
+Inicio_25s:
+    MOV R2, Delay025s
+    MOV R3, #250
+    DJNZ R3, $
+    DJNZ R2, Inicio_25s
+
+    RET
+Inicio_1s:
+    MOV R3,Delay1s
+    MOV R3, #250
+    DJNZ R2, $
+    DJNZ R3, Inicio_1s
+
+    RET
+
+END
